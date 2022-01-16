ea0af5143b372ad2f8de29335441eb32d9c87aa5
Vatsal <vatsal.acct01@gmail.com>
2022-01-16 22:49:35 +0530

    update .js .css .html

--- app.js
+++ app.js
@@ -1,358 +1,239 @@
-// FIRST TIME AFTER RELOAD
-let isFirstTime = true;
-
-//ONLOAD
+let empForm = document.empForm;
+let editAt = -1;
+const dispErrors = document.querySelectorAll(".error");
+//ON RELOAD
 window.onload = (event) => {
-  updateTable();
-  event.preventDefault();
+    isStorageEmpty() ? "" : updateTable();
+    event.preventDefault();
 };
-
-//INDEX OF ROW TO EDIT
-let editIndex = -1;
-
-// CHECK LOCAL STORAGE IS EMPTY
-function isEmpty() {
-  if (!JSON.parse(localStorage.getItem("Emp-Data"))) {
-    return true;
-  } else {
-    if (JSON.parse(localStorage.getItem("Emp-Data")).length == 0) {
-      return true;
-    } else {
-      return false;
-    }
-  }
-}
-
-// ON FORM SUBMIT
+//ON SUBMIT
 function onFormSubmit(event) {
-  // ARRAY OF HOBBIES
-  let hobbies = new Array();
-  document.querySelectorAll(".hobbies:checked").forEach((checkedHobby) => {
-    hobbies.push(checkedHobby.value);
-  });
-  let phone = document.form.phone.value ? document.form.phone.value : "";
-
-  //EMPLOYEE CLASS
-  let Employee = {
-    fname: document.form.name.value,
-    gender: document.form.gender.value,
-    dob: document.form.dob.value,
-    email: document.form.email.value,
-    phone: phone,
-    hobbbies: hobbies,
-  };
-
-  const storageArr = JSON.parse(localStorage.getItem("Emp-Data"));
-
-  //UPDATE TABLE CONDITIONALLY
-  if (isEmpty()) {
-    //EMPTY LOCAL STORAGE
-    localStorage.setItem("Emp-Data", JSON.stringify([Employee]));
-    updateTable();
     event.preventDefault();
-  } else if (editIndex != -1) {
-    //IF EDITING
-    storageArr[editIndex] = Employee;
-    localStorage.setItem("Emp-Data", JSON.stringify(storageArr));
-    updateTable();
-    event.preventDefault();
-  } else {
-    //NOT EMPTY LOCAL STORAGE
-    const newArr = [...storageArr, Employee];
-    localStorage.setItem("Emp-Data", JSON.stringify(newArr));
-    updateTable();
-    event.preventDefault();
-  }
+    if (validate()) {
+        if (isStorageEmpty()) {
+            setLocalStorage([getEmployee()]);
+        } else if (editAt != -1) {
+            let tmp = getLocalStorage();
+            tmp[editAt] = getEmployee();
+            setLocalStorage(tmp);
+            editAt = -1;
+        } else {
+            setLocalStorage([...getLocalStorage(), getEmployee()]);
+        }
+        updateTable();
+    }
+
 }
-
 //UPDATE TABLE
 function updateTable() {
+    !document.querySelector("table") ? "" : removeEle("table");
 
-  if (!isEmpty()) {
-    const allData = JSON.parse(localStorage.getItem("Emp-Data")); //GET ALL DATA
-    //BASIC TABLE
+    createEle("table", "#show-data", "table");
+    const arrOfData = getLocalStorage();
+    createEle("h3", ".table", "header", "Display")
 
-    //     //CREATING TABLE PARTS
-    //     let table = document.createElement("table");
-    //     let tableRow = new Array();
-    //     let tableHeading = new Array();
-    //     let tableData = new Array();
+    //TH
+    createEle("tr", "table", "tr" + -1);
+    const dataKeys = Object.keys(arrOfData[0]);
+    dataKeys.push("ACTION");
+    dataKeys.forEach((dataKey) => {
+        createEle("th", ".tr" + -1, "th", dataKey.toUpperCase());
+    });
 
-    //     // IS IT FIRST TIME MAKING TABLE?
-    //     if (isFirstTime) {
-    //       createTable();
-    //     } else {
-    //       document.getElementById("display-table").remove(); // DELETE EXISTING TABLE
-    //       createTable();
+    arrOfData.forEach((data, index) => {
+        //TD
+        createEle("tr", "table", "tr" + index);
+        const dataValues = Object.values(data);
+        dataValues.forEach((dataValue) => {
+            createEle("td", ".tr" + index, "td", dataValue);
+        });
+        //BTN
+        createEle("td", ".tr" + index, "td" + index);
+        setEditBtn(index, dataValues);
+        setDeleteBtn(index);
+    });
+    // for (let i = 0; i < Object.keys(arrOfData[0]).length; i++) {
+    //     for (let j = 0; j < arrOfData.length; j++) {
+    //         console.log(Object.keys(arrOfData[0]));
     //     }
+    // }
 
-    //     // CREATE TABLE
-    //     function createTable() {
-    //       document.getElementById("show-data").appendChild(table); // MAKE A NEW TABLE
-    //       table.id = "display-table";
-    //       let tableHeader = document.createElement("h3");
-    //       document.getElementById("display-table").appendChild(tableHeader);
-    //       tableHeader.classList.add("header");
-    //       tableHeader.innerHTML = "Display";
-
-    //       //ADD TH
-    //       const dataKeys = Object.keys(allData[0]);
-    //       tableRow[0] = document.createElement("tr");
-    //       dataKeys.forEach((dataKey, i) => {
-    //         tableHeading[i] = document.createElement("th");
-    //         tableHeading[i].innerHTML = dataKey.toUpperCase();
-    //         tableRow[0].appendChild(tableHeading[i]);
-    //       });
-    //       tableHeading[dataKeys.length] = document.createElement("th");
-    //       tableHeading[dataKeys.length].innerHTML = "ACTION";
-    //       tableRow[0].appendChild(tableHeading[dataKeys.length]);
-    //       table.appendChild(tableRow[0]);
-    //       isFirstTime = false;
-    //     }
-
-    //     // ADD TD & BUTTONS
-    //     allData.forEach((data, index) => {
-    //       const dataValues = Object.values(data);
-
-    //       // ADD TD
-    //       tableRow[index] = document.createElement("tr");
-    //       dataValues.forEach((dataValue, i) => {
-    //         tableData[i] = document.createElement("td");
-    //         tableData[i].innerHTML = dataValue;
-    //         tableRow[index].appendChild(tableData[i]);
-    //       });
-
-    //       tableData[dataValues.length] = document.createElement("td");
-
-    //       //CREATE EDIT BUTTON
-    //       const editBtn = document.createElement("button");
-    //       editBtn.classList.add("editBtn");
-    //       editBtn.innerHTML = "Edit";
-    //       editBtn.onclick = () => {
-    //         document.form.name.focus();
-    //         //ADDING THINGS IN FORM TO EDIT
-    //         document.form.name.value = dataValues[0];
-    //         document.form.gender.value = dataValues[1];
-    //         document.form.dob.value = dataValues[2];
-    //         document.form.email.value = dataValues[3];
-    //         document.form.phone.value = dataValues[4];
-    //         let hobbyList = document.form.hobbies; // HOBBY IS ARRAY SO...
-    //         hobbyList.forEach((hobby) => {
-    //           if (dataValues[5].includes(hobby.value)) {
-    //             hobby.checked = true;
-    //           } else {
-    //             hobby.checked = false;
-    //           }
-    //         });
-    //         editIndex = index;
-    //         isFirstTime = false;
-    //         console.log(editIndex);
-    //       };
-
-    //       //CREATE DELETE BUTTON
-    //       tableRow[dataValues.length] = document.createElement("tr");
-    //       const deleteBtn = document.createElement("button");
-    //       deleteBtn.classList.add("deleteBtn");
-    //       deleteBtn.innerHTML = "Delete";
-    //       deleteBtn.onclick = () => {
-    //         delIndex = index;
-    //         deleteRow(delIndex);
-    //       };
-
-    //       tableData[dataValues.length].appendChild(editBtn);
-    //       tableData[dataValues.length].appendChild(deleteBtn);
-    //       tableRow[index].appendChild(tableData[dataValues.length]); //ADD BUTTONS TO TABLE ROW
-
-    //       function deleteRow(delIndex) {
-    //         tableRow[delIndex].remove();
-    //         allData.splice(delIndex, 1);
-    //         localStorage.setItem("Emp-Data", JSON.stringify(allData));
-    //         if (isEmpty()) {
-    //           isFirstTime = true;
-    //           document.getElementById("display-table").remove(); // DELETE EXISTING TABLE
-    //         }
-    //       }
-
-    //       table.appendChild(tableRow[index]);
-    //     });
-    //     document.form.reset(); // RESET FORM
-    //     isFirstTime = false; // NOT FIRST TIME
-
-    //ADVANCE TABLE
-
-    //CREATING TABLE PARTS
-    let tableHeight = JSON.parse(localStorage.getItem("Emp-Data")).length;
-    let table = document.createElement("table");
-    let tableRow = new Array();
-    let tableHeading = new Array();
-    let tableData = new Array();
-
-    // IS IT FIRST TIME MAKING TABLE?
-    if (isFirstTime) {
-      createTable();
+    empForm.reset();
+}
+//MAKE EDIT BTN
+function setEditBtn(editIndex, dataValues) {
+    createEle("button", ".td" + editIndex, "editBtn" + editIndex, "Edit");
+    document.querySelector(".editBtn" + editIndex).addEventListener("click", () => {
+        editAt = editIndex;
+        setForm(dataValues);
+    });
+}
+//MAKE DELETE BTN
+function setDeleteBtn(deleteIndex) {
+    createEle("button", ".td" + deleteIndex, "deleteBtn" + deleteIndex, "Delete");
+    document.querySelector(".deleteBtn" + deleteIndex).addEventListener("click", () => {
+        let tmp = getLocalStorage();
+        tmp.splice(deleteIndex, 1);
+        setLocalStorage(tmp);
+        isStorageEmpty() ? removeEle('table') : updateTable();
+    });
+}
+//CREATE ELEMENT (HELPER)
+function createEle(element, parent, eleClass, innerHTML = "") {
+    element = document.createElement(element);
+    document.querySelector(parent).appendChild(element);
+    element.classList.add(eleClass);
+    element.id = getUUID();
+    element.innerHTML = innerHTML;
+}
+//REMOVE ELEMENT (HELPER)
+function removeEle(element) {
+    document.querySelector(element).remove();
+}
+//GET EMPLOYEE OBJECT 
+function getEmployee() {
+    const Employee = {
+        fname: empForm.fname.value,
+        gender: empForm.gender.value,
+        dob: empForm.dob.value,
+        email: empForm.email.value,
+        phone: empForm.phone.value ? empForm.phone.value : "",
+        hobbies: getHobbies(),
+    };
+    return Employee;
+}
+//SET FORM
+function setForm(dataValues) {
+    empForm.fname.focus();
+    empForm.fname.value = dataValues[0];
+    empForm.gender.value = dataValues[1];
+    empForm.dob.value = dataValues[2];
+    empForm.email.value = dataValues[3];
+    empForm.phone.value = dataValues[4];
+    document.querySelectorAll('.hobbies').forEach((hobby) => {
+        if (dataValues[5].includes(hobby.value)) {
+            hobby.checked = true;
+        } else {
+            hobby.checked = false;
+        }
+    });
+}
+//IS LOCAL STORAGE EMPTY (HELPER)
+function isStorageEmpty() {
+    if (!getLocalStorage()) {
+        return true;
     } else {
-      document.getElementById("display-table").remove(); // DELETE EXISTING TABLE
-      createTable();
+        if (getLocalStorage().length == 0) {
+            return true;
+        } else {
+            return false;
+        }
     }
-
-    // CREATE TABLE
-    function createTable() {
-      document.getElementById("show-data").appendChild(table); // MAKE A NEW TABLE
-      table.id = "advance-table";
-      let tableHeader = document.createElement("h3");
-      document.getElementById("display-table").appendChild(tableHeader);
-      tableHeader.classList.add("header");
-      tableHeader.innerHTML = "Advance";
-
-      for(let i = 0; i< tableHeight; i++){        
-        tableRow[0] = document.createElement("tr");
-      }
-
-      //ADD TH
-      const dataKeys = Object.keys(allData[0]);
-      tableRow[0] = document.createElement("tr");
-      dataKeys.forEach((dataKey, i) => {
-        tableHeading[i] = document.createElement("th");
-        tableHeading[i].innerHTML = dataKey.toUpperCase();
-        tableRow[0].appendChild(tableHeading[i]);
-      });
-      tableHeading[dataKeys.length] = document.createElement("th");
-      tableHeading[dataKeys.length].innerHTML = "ACTION";
-      tableRow[0].appendChild(tableHeading[dataKeys.length]);
-      table.appendChild(tableRow[0]);
-      isFirstTime = false;
-    }
-  }
-  // NO DATA IN LOCAL STORAGE
-  else {
-    if (!isFirstTime) {
-      document.getElementById("display-table").remove(); // DELETE EXISTING TABLE
-    }
-  }
 }
-
-// VALIDATE FUNCTIONS START //
-
-//VALIDATE ON SUBMIT
+//GET LOCAL STORAGE (HELPER)
+function getLocalStorage() {
+    return JSON.parse(localStorage.getItem("Employee"));
+}
+//SET LOCAL STORAGE (HELPER)
+function setLocalStorage(item) {
+    localStorage.setItem("Employee", JSON.stringify(item));
+}
+// RETURN HOBBIES (HELPER)
+function getHobbies() {
+    let hobbies = new Array();
+    document.querySelectorAll(".hobbies:checked").forEach((checkedHobby) => {
+        hobbies.push(checkedHobby.value);
+    });
+    return hobbies;
+}
+//UUID (HELPER)
+function getUUID() {
+    var dt = new Date().getTime();
+    var uuid = "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(
+        /[xy]/g,
+        function (c) {
+            var r = (dt + Math.random() * 16) % 16 | 0;
+            dt = Math.floor(dt / 16);
+            return (c == "x" ? r : (r & 0x3) | 0x8).toString(16);
+        }
+    );
+    return uuid;
+}
+//VALIDATE
 function validate() {
-  let nameValiRes = validateName(document.form.name);
-  let dobValiRes = validateDOB(document.form.dob);
-  let emailValiRes = validateEmail(document.form.email);
+    let validateRes = [validateName(empForm.fname.value), validateDOB(empForm.dob.value), validateEmail(empForm.email.value), validatePhone(empForm.phone.value)];
+    const inputs = document.querySelectorAll("input");
+    inputs.forEach((input) => {
+        input.addEventListener("blur", (ele) => {
+            switch (ele.target.name) {
+                case "fname": validateRes[0] = validateName(ele.target.value);
+                    break;
+                case "dob": validateRes[1] = validateDOB(ele.target.value);
+                    break;
+                case "email": validateRes[2] = validateEmail(ele.target.value);
+                    break;
+                case "phone":  validateRes[3] = validatePhone(ele.target.value);
+                    break;
+            }
+        });
+    });
 
-  if (nameValiRes && dobValiRes && emailValiRes) {
-    // CHECK IF ALL IS GOOD
-    return true;
-  } else {
-    return false;
-  }
+    return validateRes.every((res) => {
+        return res == true;
+    });
 }
-
-// VALIDATE ON INPUT
-const inputFields = document.querySelectorAll("input");
-inputFields.forEach((list) => {
-  list.addEventListener("input", (ele) => {
-    if (ele != undefined) {
-      switch (ele.target.name) {
-        case "name":
-          validateName(document.form.name);
-          break;
-        case "dob":
-          validateDOB(document.form.dob);
-          break;
-        case "email":
-          validateEmail(document.form.email);
-          break;
-        case "phone":
-          validatePhone(document.form.phone);
-          break;
-      }
-    }
-  });
-});
 
 // NAME VALIDATION
 function validateName(inputText) {
-  const showError = document.querySelector(".error-name");
-
-  if (isInputEmpty(inputText)) {
-    showError.innerHTML = "*Name required";
-    disableSubmit();
-    return false;
-  } else if (inputText.value.length > 20 || inputText.value.length < 4) {
-    showError.innerHTML = "*Name length should be 4 to 20 characters";
-    disableSubmit();
-    return false;
-  } else {
-    showError.innerHTML = "";
-    enableSubmit();
-    return true;
-  }
+    if (isLength(inputText)) {
+        dispErrors[0].innerHTML = "*Required";
+    } else if (inputText.length > 20 || inputText.length < 4) {
+        dispErrors[0].innerHTML = "*Name length should be 4 to 20 characters";
+    } else {
+        dispErrors[0].innerHTML = "";
+    }
+    return handleError(isLength(dispErrors[0].innerHTML));
 }
-
 // DOB VALIDATION
 function validateDOB(inputText) {
-  const showError = document.querySelector(".error-dob");
-  var dateformat =
-    /^((0?[1-9]|1[012])[- /.](0?[1-9]|[12][0-9]|3[01])[- /.](19|20)?[0-9]{2})*$/;
-
-  if (isInputEmpty(inputText)) {
-    showError.innerHTML = "*Date required";
-    disableSubmit();
-    return false;
-  } else if (!inputText.value.match(dateformat)) {
-    showError.innerHTML = "*Invalid date";
-    disableSubmit();
-    return false;
-  } else {
-    showError.innerHTML = "";
-    enableSubmit();
-    return true;
-  }
+    const dateformat = /^((0?[1-9]|1[012])[/](0?[1-9]|[12][0-9]|3[01])[/](19|20)?[0-9]{2})*$/;
+    if (isLength(inputText)) {
+        console.log('here');
+        dispErrors[1].innerHTML = "*Required";
+    } else if (!inputText.match(dateformat)) {
+        dispErrors[1].innerHTML = "*Invalid date (MM/DD/YY)";
+    } else {
+        dispErrors[1].innerHTML = "";
+    }
+    return handleError(isLength(dispErrors[1].innerHTML));
 }
-
 // EMAIL VALIDATION
 function validateEmail(inputText) {
-  const showError = document.querySelector(".error-email");
-  var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-
-  if (isInputEmpty(inputText)) {
-    showError.innerHTML = "*Email required";
-    disableSubmit();
-    return false;
-  } else if (!inputText.value.match(mailformat)) {
-    showError.innerHTML = "*Invalid Email";
-    disableSubmit();
-    return false;
-  } else {
-    showError.innerHTML = "";
-    enableSubmit();
-    return true;
-  }
+    const mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if (isLength(inputText)) {
+        dispErrors[2].innerHTML = "*Required";
+    } else if (!inputText.match(mailformat)) {
+        dispErrors[2].innerHTML = "*Invalid Email (eg. work@email.com)";
+    } else {
+        dispErrors[2].innerHTML = "";
+    }
+    return handleError(isLength(dispErrors[2].innerHTML));
 }
-
 // PHONE VALIDATION
 function validatePhone(inputText) {
-  const showError = document.querySelector(".error-phone");
-  if (inputText.value.length != 10) {
-    showError.innerHTML = "*Invalid Phone";
-    disableSubmit();
-  } else if (isEmpty(inputText) || inputText.value.length == 10) {
-    showError.innerHTML = "";
-    enableSubmit();
-  }
+    if (isLength(inputText) || isLength(inputText, 10)) {
+        dispErrors[3].innerHTML = "";
+    } else if (!isLength(inputText, 10)) {
+        dispErrors[3].innerHTML = "*Phone number should be of 10 digits";
+    }
+    return handleError(isLength(dispErrors[3].innerHTML));
 }
-
-// VALIDATE FUNCTIONS END //
-
-// DISABLE/ENABLE SUBMIT
-function disableSubmit() {
-  document.getElementById("submit-btn").disabled = true;
+// HANDLE ERROR (HELPER)
+function handleError(isGood) {
+    document.getElementById("submit-btn").disabled = !isGood;
+    return isGood;
 }
-function enableSubmit() {
-  document.getElementById("submit-btn").disabled = false;
-}
-
-// IS INPUT EMPTY?
-function isInputEmpty(inputText) {
-  return inputText.value == "" || inputText.value == null;
+// IS INPUT LENGTH (HELPER)
+function isLength(inputText, length = 0) {
+    return inputText.length == length;
 }
